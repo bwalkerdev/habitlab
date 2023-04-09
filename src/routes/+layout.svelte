5871e87e2638f7e9c46d76be35d96f06b251e359
Bryce Walker <99450073+papabryce@users.noreply.github.com>
2023-04-08 19:14:19 -0600

    Built chip system
    Built chip system. Need to put in separate component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,24 @@
 	import '@skeletonlabs/skeleton/styles/all.css';
 	// Most of your app wide CSS should be put in this file
 	import '../app.postcss';
+	import { AppShell, AppBar, AppRail, AppRailTile } from '@skeletonlabs/skeleton';
+	import { writable } from 'svelte/store';
+	import type { Writable } from 'svelte/store';
+	const storeValue: Writable<number> = writable(0);
+	import Icon from '@iconify/svelte';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
 </script>
 
-<slot />
+<AppShell>
+	<svelte:fragment slot="sidebarLeft">
+		<AppRail selected={storeValue} class="rounded-br-lg">
+			<AppRailTile value={0}><Icon icon="ci:house-01" width="50" height="50" /></AppRailTile>
+			<AppRailTile value={1}><Icon icon="ci:table" width="50" height="50" /></AppRailTile>
+			<AppRailTile value={2}><Icon icon="ci:settings" width="50" height="50" /></AppRailTile>
+			<svelte:fragment slot="trail"><LightSwitch class="mx-4 my-5" /></svelte:fragment>
+		</AppRail>
+	</svelte:fragment>
+	<!-- Router Slot -->
+	<slot />
+	<!-- ---- / ---- -->
+</AppShell>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,78 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+	let selected = '';
+	let newChipName = '';
 
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-5">
-		<h1>Let's get cracking bones!</h1>
-		<p>Start by exploring:</p>
-		<ul>
-			<li><code>/src/routes/+layout.svelte</code> - barebones layout, the CSS import order is critical!</li>
-			<li><code>/src/app.postcss</code> - minimal css to make the page full screen, may not be relevant for your project</li>
-			<li><code>/src/routes/+page.svelte</code> - this page, you can replace the contents</li>
-		</ul>
+	let randomColor = () => {
+		let color = '#';
+		for (let i = 0; i < 6; i++) {
+			color += Math.floor(Math.random() * 16).toString(16);
+		}
+		return color;
+	};
+
+	let chipColor = randomColor();
+
+	let chipArr = [
+		{ task: 'Sleep', color: 'red' },
+		{ task: 'Eat', color: 'blue' },
+		{ task: 'Girlfriend', color: 'green' },
+		{ task: 'School', color: 'orange' }
+	];
+	function pushChipArr() {
+		chipArr = [...chipArr, { task: newChipName, color: chipColor }];
+		newChipName = '';
+		chipColor = randomColor();
+	}
+	function removeChipArr(index: number) {
+		chipArr.splice(index, 1);
+		chipArr = [...chipArr];
+	}
+</script>
+
+<div class="flex flex-row">
+	<div class="basis-1/4 m-5"><h3>Good Morning</h3></div>
+	<div class="grow" />
+	<div class="basis-12 m-5"><h3>2🔥</h3></div>
+</div>
+<div class="flex flex-row">
+	<div class="card p-4 ml-5 basis-3/4">
+		{#each chipArr as c, index}
+			<div
+				class="chip {selected === c.task
+					? 'variant-soft-surface chip-selected'
+					: 'chip-not-selected'} mx-1"
+				style="--theme-color: {c.color}"
+				on:click={() => {
+					selected = c.task;
+				}}
+				on:keypress
+			>
+				{c.task}
+				<button class="ml-3" on:click={() => removeChipArr(index)}
+					><Icon icon="ci:close-md" /></button
+				>
+			</div>
+		{/each}
+	</div>
+	<div class="card p-4 mx-5 basis-1/4">
+		<div class="flex flex-row">
+			<input class="input" bind:value={newChipName} />
+			<input class="rounded px-1 mx-2 variant-ghost-surface" type="color" bind:value={chipColor} />
+			<button type="button" class="btn btn-sm variant-filled" on:click={pushChipArr}
+				><Icon icon="ci:add-plus" /></button
+			>
+		</div>
 	</div>
 </div>
+
+<style>
+	.chip-not-selected {
+		background-color: var(--theme-color);
+		color: #ffffff;
+	}
+	.chip-selected {
+		border-color: var(--theme-color);
+		border-width: 2px;
+	}
+</style>
