7c2072339502bdd56c0534431035a727c4cb919b
Bryce Walker <99450073+papabryce@users.noreply.github.com>
2023-04-18 19:36:43 -0600

    Cleaned up Global scrollbar CSS

--- src/lib/TaskSelector.svelte
+++ src/lib/TaskSelector.svelte
@@ -104,23 +104,4 @@
 		border: #ffffff 2px solid;
 		border-radius: 10px;
 	}
-	/* width */
-	::-webkit-scrollbar {
-		height: 5px;
-	}
-
-	/* Track */
-	::-webkit-scrollbar-track {
-		background: #f1f1f1;
-	}
-
-	/* Handle */
-	::-webkit-scrollbar-thumb {
-		background: #888;
-	}
-
-	/* Handle on hover */
-	::-webkit-scrollbar-thumb:hover {
-		background: #555;
-	}
 </style>
